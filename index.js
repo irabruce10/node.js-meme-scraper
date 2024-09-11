f5ece58b3548c2c9167aec6b249e6b51d2284901
Bruce <irabruce10@gmail.com>
2024-09-11 17:35:22 +0200

    vim

---
+++ index.js
@@ -0,0 +1,10 @@
+import fetch from 'node-fetch';
+
+fetch('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((response) => response.text())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
