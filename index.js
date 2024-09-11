3ce0c20f1f35b71d2dc306df16dd0df64cfb0c73
Bruce <irabruce10@gmail.com>
2024-09-11 18:55:26 +0200

    fetching some data

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import fetch from 'node-fetch';
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
+const getData = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+)
   .then((response) => response.text())
   .then((data) => {
-    console.log('data');
+    let m,
+      urls = [],
+      str = data;
+
+    const rex = /<img[^>]+src="?([^"\s]+)"?\s*\/>/g;
+
+    while ((m = rex.exec(str))) {
+      urls.push(m[1]);
+    }
+
+    const slicedArray = urls.slice(0, 10).map((x) => {
+      const imageData = x;
+      console.log(imageData);
+    });
+
+    // console.log(data);
   })
   .catch((error) => {
     console.error(error);
