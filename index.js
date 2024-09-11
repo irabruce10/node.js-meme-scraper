d4d08c5c1fe361bed3c405a26bf5be40488b4baf
Bruce <irabruce10@gmail.com>
2024-09-11 21:20:59 +0200

    creating the fileName

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import fetch from 'node-fetch';
 
 import fs from 'node:fs';
+
+// eslint-disable-next-line import-x/no-unresolved
+import client from 'node:https';
+
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 
 const getData = await fetch(
@@ -18,23 +22,47 @@ const getData = await fetch(
       urls.push(m[1]);
     }
 
-    const slicedArray = urls.slice(0, 10).map((x, i) => {
+    urls.slice(0, 10).map((x, i) => {
       const imageData = x;
       console.log(imageData, i + 1);
 
-      const content = 'hello!';
-
-      try {
-        if (i >= 9) {
-          fs.writeFileSync(`memes/${i + 1}.jpg`, content);
-        } else {
-          fs.writeFileSync(`memes/0${i + 1}.jpg`, content);
-        }
-
-        // file written successfully
-      } catch (err) {
-        console.error(err);
+      function downloadImage(url, filepath) {
+        return new Promise((resolve, reject) => {
+          client.get(url, (res) => {
+            if (res.statusCode === 200) {
+              res
+                .pipe(fs.createWriteStream(filepath))
+                .on('error', reject)
+                .once('close', () => resolve(filepath));
+            } else {
+              // Consume response data to free up memory
+              res.resume();
+              reject(
+                new Error(
+                  `Request Failed With a Status Code: ${res.statusCode}`,
+                ),
+              );
+            }
+          });
+        });
       }
+      downloadImage(imageData, `memes/0${i + 1}.jpg`)
+        .then(console.log)
+        .catch(console.error);
+
+      // const content = 'hello!';
+
+      // try {
+      //   if (i >= 9) {
+      //     fs.writeFileSync(`memes/${i + 1}.jpg`, content);
+      //   } else {
+      //     fs.writeFileSync(`memes/0${i + 1}.jpg`, content);
+      //   }
+
+      //   // file written successfully
+      // } catch (err) {
+      //   console.error(err);
+      // }
 
       // imageData.forEach((el) => {
       //   const content = 'hello!';
