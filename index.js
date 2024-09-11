dc7e12921f9dfcd52f20b02d0a4f85120095fe32
Bruce <irabruce10@gmail.com>
2024-09-11 20:24:12 +0200

    fetching some data

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch';
 
+import fs from 'node:fs';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+
 const getData = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 )
@@ -15,9 +18,35 @@ const getData = await fetch(
       urls.push(m[1]);
     }
 
-    const slicedArray = urls.slice(0, 10).map((x) => {
+    const slicedArray = urls.slice(0, 10).map((x, i) => {
       const imageData = x;
-      console.log(imageData);
+      console.log(imageData, i + 1);
+
+      const content = 'hello!';
+
+      try {
+        if (i >= 9) {
+          fs.writeFileSync(`memes/${i + 1}.jpg`, content);
+        } else {
+          fs.writeFileSync(`memes/0${i + 1}.jpg`, content);
+        }
+
+        // file written successfully
+      } catch (err) {
+        console.error(err);
+      }
+
+      // imageData.forEach((el) => {
+      //   const content = 'hello!';
+      //   const p = el;
+
+      //   try {
+      //     fs.writeFileSync(`memes/${p.length}.jpg`, content);
+      //     // file written successfully
+      //   } catch (err) {
+      //     console.error(err);
+      //   }
+      // });
     });
 
     // console.log(data);
