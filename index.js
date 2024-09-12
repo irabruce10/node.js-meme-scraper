ab3d472fa1c00d857544a02fcd1c9dddef40f1f3
Bruce <irabruce10@gmail.com>
2024-09-12 14:42:55 +0200

    Fixing some bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 import fs from 'node:fs';
 
-import client from 'node:https';
+// import client from 'node:https';
 
 await fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => response.text())
@@ -17,41 +17,58 @@ await fetch('https://memegen-link-examples-upleveled.netlify.app/')
       urls.push(m[1]);
     }
 
-    urls.slice(0, 10).forEach(function (x, i) {
-      const imageData = x;
-      console.log(imageData, i + 1);
+    const imageUrlData = urls.slice(0, 10);
+    console.log(imageUrlData);
 
-      function downloadImage(url, filepath) {
-        return new Promise((resolve, reject) => {
-          client.get(url, (res) => {
-            if (res.statusCode === 200) {
-              res
-                .pipe(fs.createWriteStream(filepath))
-                .on('error', reject)
-                .once('close', () => resolve(filepath));
-            } else {
-              res.resume();
-              reject(
-                new Error(
-                  `Request Failed With a Status Code: ${res.statusCode}`,
-                ),
-              );
-            }
-          });
-        });
-      }
+    imageUrlData.forEach((url, i) => {
+      console.log(url);
+      const content = url;
 
       if (i >= 9) {
-        downloadImage(imageData, `memes/${i + 1}.jpg`)
-          .then(console.log)
-          .catch(console.error);
+        fs.writeFileSync(`memes/${i + 1}.jpg`, content);
       } else {
-        console.log('Downloading ');
-        downloadImage(imageData, `memes/0${i + 1}.jpg`)
-          .then(console.log)
-          .catch(console.error);
+        fs.writeFileSync(`memes/0${i + 1}.jpg`, content);
       }
     });
+
+    // loop the 10url
+    // and download the images to the 'memes' directory, naming them sequentially from 1 to 10.
+
+    // urls.slice(0, 10).forEach(function (x, i) {
+    //   const imageData = x;
+    //   console.log(imageData, i + 1);
+
+    //   function downloadImage(url, filepath) {
+    //     return new Promise((resolve, reject) => {
+    //       client.get(url, (res) => {
+    //         if (res.statusCode === 200) {
+    //           res
+    //             .pipe(fs.createWriteStream(filepath))
+    //             .on('error', reject)
+    //             .once('close', () => resolve(filepath));
+    //         } else {
+    //           res.resume();
+    //           reject(
+    //             new Error(
+    //               `Request Failed With a Status Code: ${res.statusCode}`,
+    //             ),
+    //           );
+    //         }
+    //       });
+    //     });
+    //   }
+
+    //   if (i >= 9) {
+    //     downloadImage(imageData, `memes/${i + 1}.jpg`)
+    //       .then(console.log)
+    //       .catch(console.error);
+    //   } else {
+    //     console.log('Downloading ');
+    //     downloadImage(imageData, `memes/0${i + 1}.jpg`)
+    //       .then(console.log)
+    //       .catch(console.error);
+    //   }
+    // });
   })
   .catch((error) => {
     console.error(error);
